6183b5966309cb40a20fe7b7f27fb675cc57df07
Justin Parker <jparkrr@users.noreply.github.com>
2014-07-27 19:02:40 -0600

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+# Download and install Command Line Tools
+if [[ ! -x /usr/bin/gcc ]]; then
+    echo "Info   | Install   | xcode"
+    xcode-select --install
+fi
+
+# Download and install Homebrew
+if [[ ! -x /usr/local/bin/brew ]]; then
+    echo "Info   | Install   | homebrew"
+    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+fi
+
+# Download and install Ansible
+if [[ ! -x /usr/local/bin/ansible ]]; then
+    brew install ansible
+fi
+
+# Provision the box
+ansible-playbook --ask-sudo-pass -i inventories/osx site.yml --connection=local
